984037b86904d66331c4543888648ab5b06c5c7c
Ramon Fernandez <ramon@astudillo.com>
2018-08-01 12:09:49 -0400

    Uncomment feature extraction

--- egs/librispeech/asr-unpaired/run.sh
+++ egs/librispeech/asr-unpaired/run.sh
@@ -51,7 +51,6 @@ aconv_chans=10
 aconv_filts=100
 
 # hybrid CTC/attention
-#mtlalpha=0.5
 mtlalpha=0.0
 
 # minibatch related
@@ -155,42 +154,42 @@ if [ ${stage} -le 1 ]; then
     echo "stage 1: Feature Generation"
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-#    for x in dev_clean test_clean dev_other test_other train_clean_360; do
-#        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 80 data/${x} exp/make_fbank/${x} ${fbankdir}
-#    done
+    for x in dev_clean test_clean dev_other test_other train_clean_360; do
+        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 80 data/${x} exp/make_fbank/${x} ${fbankdir}
+    done
 
-    #utils/combine_data.sh data/${train_set}_org data/train_clean_360 
+    utils/combine_data.sh data/${train_set}_org data/train_clean_360 
     utils/combine_data.sh data/${train_dev}_org data/dev_clean data/dev_other
 
     # remove utt having more than 1500 frames
     # remove utt having more than 300 characters
-    #remove_longshortdata.sh --maxframes 1500 --maxchars 300 data/${train_set}_org data/${train_set}
+    remove_longshortdata.sh --maxframes 1500 --maxchars 300 data/${train_set}_org data/${train_set}
     remove_longshortdata.sh --maxframes 1500 --maxchars 300 data/${train_dev}_org data/${train_dev}
 
     # compute global CMVN
-    #compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
+    compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
 
     # dump features for training
-#    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
-#    utils/create_split_dir.pl \
-#        /export/b{14,15,16,17}/${USER}/espnet-data/egs/librispeech/asr-unpaired/dump/${train_set}/delta${do_delta}/storage \
-#        ${feat_tr_dir}/storage
-#    fi
+    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
+    utils/create_split_dir.pl \
+        /export/b{14,15,16,17}/${USER}/espnet-data/egs/librispeech/asr-unpaired/dump/${train_set}/delta${do_delta}/storage \
+        ${feat_tr_dir}/storage
+    fi
     if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_dt_dir}/storage ]; then
     utils/create_split_dir.pl \
         /export/b{14,15,16,17}/${USER}/espnet-data/egs/librispeech/asr-unpaired/dump/${train_dev}/delta${do_delta}/storage \
         ${feat_dt_dir}/storage
     fi
-#    dump.sh --cmd "$train_cmd" --nj 80 --do_delta $do_delta \
-#        data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
+    dump.sh --cmd "$train_cmd" --nj 80 --do_delta $do_delta \
+        data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
     dump.sh --cmd "$train_cmd" --nj 80 --do_delta $do_delta \
         data/${train_dev}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
-#    for rtask in ${recog_set}; do
-#        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
-#        dump.sh --cmd "$train_cmd" --nj 80 --do_delta $do_delta \
-#            data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
-#            ${feat_recog_dir}
-#    done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj 80 --do_delta $do_delta \
+            data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
+    done
 fi
 
 dict=data/lang_1char/${train_set}_units.txt
@@ -205,15 +204,15 @@ if [ ${stage} -le 2 ]; then
     wc -l ${dict}
 
     # make json labels
-#    data2json.sh --feat ${feat_tr_dir}/feats.scp \
-#         data/${train_set} ${dict} > ${feat_tr_dir}/data.json
+    data2json.sh --feat ${feat_tr_dir}/feats.scp \
+         data/${train_set} ${dict} > ${feat_tr_dir}/data.json
     data2json.sh --feat ${feat_dt_dir}/feats.scp \
          data/${train_dev} ${dict} > ${feat_dt_dir}/data.json
-#    for rtask in ${recog_set}; do
-#        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-#        data2json.sh --feat ${feat_recog_dir}/feats.scp \
-#            data/${rtask} ${dict} > ${feat_recog_dir}/data.json
-#    done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        data2json.sh --feat ${feat_recog_dir}/feats.scp \
+            data/${rtask} ${dict} > ${feat_recog_dir}/data.json
+    done
 fi
 
 taco_feat_tr_dir=${dumpdir}/taco_${train_set}/delta${do_delta};mkdir -p ${taco_feat_tr_dir}
@@ -223,41 +222,42 @@ if [ ${stage} -le 3 ]; then
     fbankdir=taco_fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch
     # on each frame
-#    for x in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
-#        printf "make_fbank: \033[34m${x}\033[0m\n"    
-#        utils/copy_data_dir.sh data/${x} data/taco_${x}
-#        # Using librosa
-#        local/make_fbank.sh --cmd "${train_cmd}" --nj 40 \
-#            --fs ${fs} --fmax "${fmax}" --fmin "${fmin}" \
-#            --n_mels ${n_mels} --n_fft ${taco_n_fft} \
-#            --n_shift ${taco_n_shift} --win_length $taco_win_length \
-#            data/taco_${x} exp/taco_make_fbank/${x} ${fbankdir}
-#    done
+    for x in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
+        printf "make_fbank: \033[34m${x}\033[0m\n"    
+        utils/copy_data_dir.sh data/${x} data/taco_${x}
+        # Using librosa
+        local/make_fbank.sh --cmd "${train_cmd}" --nj 40 \
+            --fs ${fs} --fmax "${fmax}" --fmin "${fmin}" \
+            --n_mels ${n_mels} --n_fft ${taco_n_fft} \
+            --n_shift ${taco_n_shift} --win_length $taco_win_length \
+            data/taco_${x} exp/taco_make_fbank/${x} ${fbankdir}
+    done
 
-#    # compute global CMVN
-#    compute-cmvn-stats scp:data/taco_${train_set}/feats.scp data/taco_${train_set}/cmvn.ark
-#    dump.sh --cmd "$train_cmd" --nj 80 --do_delta false \
-#        data/taco_${train_set}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/train ${taco_feat_tr_dir}
-#    dump.sh --cmd "$train_cmd" --nj 32 --do_delta false \
-#        data/taco_${train_dev}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/dev ${taco_feat_dt_dir}
-#    for rtask in ${recog_set}; do
-#        feat_recog_dir=${dumpdir}/taco_${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
-#        dump.sh --cmd "$train_cmd" --nj 32 --do_delta false \
-#            data/taco_${rtask}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
-#            ${feat_recog_dir}
-#    done
+    # compute global CMVN
+    compute-cmvn-stats scp:data/taco_${train_set}/feats.scp data/taco_${train_set}/cmvn.ark
+
+    dump.sh --cmd "$train_cmd" --nj 80 --do_delta false \
+        data/taco_${train_set}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/train ${taco_feat_tr_dir}
+    dump.sh --cmd "$train_cmd" --nj 32 --do_delta false \
+        data/taco_${train_dev}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/dev ${taco_feat_dt_dir}
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/taco_${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj 32 --do_delta false \
+            data/taco_${rtask}/feats.scp data/taco_${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
+    done
 
     # Append data to jsons
 
     # Update json
-#    python local/data_io.py \
-#        --in-scp-file data/taco_${train_set}/feats.scp \
-#        --ark-class matrix \
-#        --input-name input2 \
-#        --in-json-file ${feat_tr_dir}/data.json \
-#        --action add-scp-data-to-input \
-#        --verbose 1 \
-#        --force
+    python local/data_io.py \
+        --in-scp-file data/taco_${train_set}/feats.scp \
+        --ark-class matrix \
+        --input-name input2 \
+        --in-json-file ${feat_tr_dir}/data.json \
+        --action add-scp-data-to-input \
+        --verbose 1 \
+        --force
 
     python local/data_io.py \
         --in-scp-file data/taco_${train_dev}/feats.scp \
@@ -268,17 +268,17 @@ if [ ${stage} -le 3 ]; then
         --verbose 1 \
         --force
 
-#    for rtask in ${recog_set}; do
-#        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-#        python local/data_io.py \
-#            --in-scp-file data/taco_${rtask}/feats.scp \
-#            --ark-class matrix \
-#            --input-name input2 \
-#            --in-json-file $feat_recog_dir/data.json \
-#            --action add-scp-data-to-input \
-#            --verbose 1 \
-#            --force
-#    done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        python local/data_io.py \
+            --in-scp-file data/taco_${rtask}/feats.scp \
+            --ark-class matrix \
+            --input-name input2 \
+            --in-json-file $feat_recog_dir/data.json \
+            --action add-scp-data-to-input \
+            --verbose 1 \
+            --force
+    done
 
 fi
 
@@ -288,45 +288,45 @@ if [ ${stage} -le 4 ]; then
     mfccdir=mfcc
     vaddir=mfcc
     # FIXME: This should take and scp as input
-#    for name in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
-#        printf "make_fbank: \033[34m${name}\033[0m\n"    
-#        utils/copy_data_dir.sh data/${name} data/${name}_mfcc
-#        steps/make_mfcc.sh \
-#            --write-utt2num-frames true \
-#            --mfcc-config conf/mfcc.conf \
-#            --nj 40 --cmd "$train_cmd" \
-#            data/${name}_mfcc exp/make_mfcc $mfccdir
-#        utils/fix_data_dir.sh data/${name}_mfcc
-#        sid/compute_vad_decision.sh --nj 41 --cmd "$train_cmd" \
-#            data/${name}_mfcc exp/make_vad ${vaddir}
-#        utils/fix_data_dir.sh data/${name}_mfcc
-#    done
+    for name in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
+        printf "make_fbank: \033[34m${name}\033[0m\n"    
+        utils/copy_data_dir.sh data/${name} data/${name}_mfcc
+        steps/make_mfcc.sh \
+            --write-utt2num-frames true \
+            --mfcc-config conf/mfcc.conf \
+            --nj 40 --cmd "$train_cmd" \
+            data/${name}_mfcc exp/make_mfcc $mfccdir
+        utils/fix_data_dir.sh data/${name}_mfcc
+        sid/compute_vad_decision.sh --nj 41 --cmd "$train_cmd" \
+            data/${name}_mfcc exp/make_vad ${vaddir}
+        utils/fix_data_dir.sh data/${name}_mfcc
+    done
     # Check pretrained model existence
     nnet_dir=exp/xvector_nnet_1a
-#    if [ ! -e $nnet_dir ];then
-#        echo "X-vector model does not exist. Download pre-trained model."
-#        wget http://kaldi-asr.org/models/8/0008_sitw_v2_1a.tar.gz
-#        tar xvf 0008_sitw_v2_1a.tar.gz
-#        mv 0008_sitw_v2_1a/exp/xvector_nnet_1a exp
-#        rm -rf 0008_sitw_v2_1a.tar.gz 0008_sitw_v2_1a
-#    fi
-#    # Extract x-vector
-#    for name in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
-#        printf "x-vector: \033[34m${name}\033[0m\n"    
-#        sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 4G" --nj 40 \
-#            $nnet_dir data/${name}_mfcc \
-#            $nnet_dir/xvectors_${name}
-#    done
+    if [ ! -e $nnet_dir ];then
+        echo "X-vector model does not exist. Download pre-trained model."
+        wget http://kaldi-asr.org/models/8/0008_sitw_v2_1a.tar.gz
+        tar xvf 0008_sitw_v2_1a.tar.gz
+        mv 0008_sitw_v2_1a/exp/xvector_nnet_1a exp
+        rm -rf 0008_sitw_v2_1a.tar.gz 0008_sitw_v2_1a
+    fi
+    # Extract x-vector
+    for name in ${train_set} ${train_dev} test_clean test_other dev_clean dev_other; do
+        printf "x-vector: \033[34m${name}\033[0m\n"    
+        sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 4G" --nj 40 \
+            $nnet_dir data/${name}_mfcc \
+            $nnet_dir/xvectors_${name}
+    done
 
     # Update json
-#    python local/data_io.py \
-#        --in-scp-file ${nnet_dir}/xvectors_${train_set}/xvector.scp \
-#        --ark-class vector \
-#        --input-name input3 \
-#        --in-json-file ${feat_tr_dir}/data.json \
-#        --action add-scp-data-to-input \
-#        --verbose 1 \
-#        --force
+    python local/data_io.py \
+        --in-scp-file ${nnet_dir}/xvectors_${train_set}/xvector.scp \
+        --ark-class vector \
+        --input-name input3 \
+        --in-json-file ${feat_tr_dir}/data.json \
+        --action add-scp-data-to-input \
+        --verbose 1 \
+        --force
 
     python local/data_io.py \
         --in-scp-file ${nnet_dir}/xvectors_${train_dev}/xvector.scp \
@@ -337,47 +337,47 @@ if [ ${stage} -le 4 ]; then
         --verbose 1 \
         --force
 
-#    for rtask in ${recog_set}; do
-#        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-#        python local/data_io.py \
-#            --in-scp-file ${nnet_dir}/xvectors_${rtask}/xvector.scp \
-#            --ark-class vector \
-#            --input-name input3 \
-#            --in-json-file $feat_recog_dir/data.json \
-#            --action add-scp-data-to-input \
-#            --verbose 1 \
-#            --force
-#    done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        python local/data_io.py \
+            --in-scp-file ${nnet_dir}/xvectors_${rtask}/xvector.scp \
+            --ark-class vector \
+            --input-name input3 \
+            --in-json-file $feat_recog_dir/data.json \
+            --action add-scp-data-to-input \
+            --verbose 1 \
+            --force
+    done
 
 fi
 
 # You can skip this and remove --rnnlm option in the recognition (stage 5)
 lmexpdir=exp/train_rnnlm_2layer_bs256
-#mkdir -p ${lmexpdir}
-#if [ ${stage} -le 5 ]; then
-#    echo "stage 5: LM Preparation"
-#    lmdatadir=data/local/lm_train
-#    mkdir -p ${lmdatadir}
-#    text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
-#        > ${lmdatadir}/train.txt
-#    text2token.py -s 1 -n 1 data/${train_dev}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
-#        > ${lmdatadir}/valid.txt
-#    # use only 1 gpu
-#    if [ ${ngpu} -gt 1 ]; then
-#        echo "LM training does not support multi-gpu. signle gpu will be used."
-#    fi
-#    ${cuda_cmd} ${lmexpdir}/train.log \
-#        lm_train.py \
-#        --ngpu ${ngpu} \
-#        --backend ${backend} \
-#        --verbose 1 \
-#        --outdir ${lmexpdir} \
-#        --train-label ${lmdatadir}/train.txt \
-#        --valid-label ${lmdatadir}/valid.txt \
-#        --epoch 60 \
-#        --batchsize 256 \
-#        --dict ${dict}
-#fi
+mkdir -p ${lmexpdir}
+if [ ${stage} -le 5 ]; then
+    echo "stage 5: LM Preparation"
+    lmdatadir=data/local/lm_train
+    mkdir -p ${lmdatadir}
+    text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
+        > ${lmdatadir}/train.txt
+    text2token.py -s 1 -n 1 data/${train_dev}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
+        > ${lmdatadir}/valid.txt
+    # use only 1 gpu
+    if [ ${ngpu} -gt 1 ]; then
+        echo "LM training does not support multi-gpu. signle gpu will be used."
+    fi
+    ${cuda_cmd} ${lmexpdir}/train.log \
+        lm_train.py \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --verbose 1 \
+        --outdir ${lmexpdir} \
+        --train-label ${lmdatadir}/train.txt \
+        --valid-label ${lmdatadir}/valid.txt \
+        --epoch 60 \
+        --batchsize 256 \
+        --dict ${dict}
+fi
 
 if [ -z ${tag} ]; then
     expdir=exp/${train_set}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_d${dlayers}_unit${dunits}_${atype}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_bs${batchsize}_mli${maxlen_in}_mlo${maxlen_out}
